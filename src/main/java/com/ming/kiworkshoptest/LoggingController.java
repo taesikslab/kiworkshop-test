ba45730528f25f3782555134c39e95f4bf2aefdb
minzzzy <minzzzy@woowahan.com>
2020-10-02 22:23:13 +0900

    Add appenders for log level and LoggingController

---
+++ src/main/java/com/ming/kiworkshoptest/LoggingController.java
@@ -0,0 +1,35 @@
+package com.ming.kiworkshoptest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class LoggingController {
+    @GetMapping("/info")
+    public String info() {
+        log.info("info api log");
+        return "INFO";
+    }
+
+    @GetMapping("/warn")
+    public String warn() {
+        log.warn("warn api log");
+        return "WARN";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        log.error("error api log");
+        return "ERROR";
+    }
+
+    @GetMapping("/debug")
+    public String debug() {
+        log.debug("debug api log");
+        return "DEBUG";
+    }
+}
